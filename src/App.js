3c7eef9f762d249018d2b56698444f9bc2abb430
Yefer Enrique García Moncada <gm.moncada@gmail.com>
2024-08-13 16:54:44 -0500

    First project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import {TodoCounter} from './TodoCounter';
+import {TodoSearch} from './TodoSearch';
+import {TodoList} from './TodoList';
+import {TodoItem} from './TodoItem';
+import {CreateTodoButton} from './CreateTodoButton';
+import React from 'react';
+
+const defaultTodos =[
+
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el curso de intro a React', completed: false},
+  {text: 'llorar con la llorona', completed: false},
+  {text: 'Speak', completed: false},
+  {text: 'Speak', completed: false},
+];
+
+function App() {
+  return (
+    <>
+
+      <TodoCounter completed={16} total={25}/>
+      <TodoSearch />
+
+      <TodoList>
+      {defaultTodos.map(todo=>( 
+        <TodoItem 
+        key={todo.text} 
+        text={todo.text}
+        completed={todo.completed}  
+        />
+      ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/TodoSearch.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import './TodoSearch.css';
+
+
+function TodoSearch() {
+
+  const[searchValue, setSearchValue] = React.useState
+  
+  return (
+    <input 
+    placeholder="Cortar cebolla"
+    className="TodoSearch"
+    onChange={(event) => {
+      console.log(event);
+      console.log(event.target);
+      console.log(event.target.value);
+
+    }}
+    />
+    );
+  }
+  export {TodoSearch};
