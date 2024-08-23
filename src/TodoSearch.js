691e166f7f884bf66797d2d481929ca2dd952d64
hostmast <=gm.moncada@gmail.com>
2024-08-23 11:38:19 -0500

    new change

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -4,14 +4,18 @@ import './TodoSearch.css';
 
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState(``);
 
-  const[searchValue, setSearchValue] = React.useState
-  
+  console.log(`Los usuario buscan todos de` + searchValue);
+
   return (
     <input 
     placeholder="Cortar cebolla"
     className="TodoSearch"
+    value={searchValue}
     onChange={(event) => {
+      setSearchValue(event.target.value);
+      console.log(`Escribe en el TodoSearch`);
       console.log(event);
       console.log(event.target);
       console.log(event.target.value);
